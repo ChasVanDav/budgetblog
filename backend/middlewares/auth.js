f8f623e6c5eee94e187508fa9f3f00863c26d486
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-18 13:13:36 -0400

    changed key

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,7 @@ const authenticateJWT = (req, res, next) => {
     const token = req.headers['authorization'];
 
     if (token) {
-        jwt.verify(token, 'your_secret_key', (err, user) => {
+        jwt.verify(token, 'my_secret_key', (err, user) => {
             if (err) {
                 return res.sendStatus(403);
             }
