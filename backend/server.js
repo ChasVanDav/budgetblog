787af5ad1fd9b2dc893f53330344b6c17e6c3d96
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-19 13:59:45 -0400

    cleaned up formatting

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+//required packages
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const pool = require('./db/pool');
+
+//configuration
+dotenv.config(); 
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
+
+//route to get all users
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Start the server
+const PORT = process.env.PORT || 4000; 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
