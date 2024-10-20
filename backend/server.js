882394b49a1450eb17c8b02e8ee133129632ef94
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-20 14:06:25 -0400

    set up user auth with jwt and bcrypt

--- backend/server.js
+++ backend/server.js
@@ -31,46 +31,45 @@ app.post('/hash-passwords', async (req, res) => {
           await pool.query('UPDATE users SET password = $1 WHERE user_id = $2', [hashedPassword, user.user_id]);
       }
 
-      // Respond with success message
       res.status(200).json({ message: 'All passwords have been hashed successfully!' });
   } catch (error) {
-      // Log the error message for debugging
       console.error('Error hashing passwords:', error.message);
-      
-      // Respond with a server error message
       res.status(500).json({ message: 'An error occurred while hashing passwords.', error: error.message });
   }
 });
 
 
 //----jwt auth----for user login//
-// app.post('/login', async (req, res) => {
-//   const { username, password } = req.body;
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
-//   try {
-//       const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-//       const user = result.rows[0];
+  try {
+      const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+      const user = result.rows[0];
+      
+      console.log('Fetched User:', user);
 
-//       if (!user) {
-//           return res.status(400).json({ message: 'Invalid credentials' });
-//       }
+      if (!user) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+      }
 
-//       const isPasswordValid = await bcrypt.compare(password, user.password);
-//       if (!isPasswordValid) {
-//           return res.status(400).json({ message: 'Invalid credentials' });
-//       }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+      }
 
-//       // Generate JWT 
-//       const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      // Generate JWT
+      const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-//       res.json({ message: 'Login successful', token });
-//   } catch (error) {
-//       console.error('Error during login:', error);
-//       res.status(500).json({ message: 'An error occurred during login.' });
-//   }
-// });
+      res.json({ message: 'Login successful', token });
+  } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ message: 'An error occurred during login.' });
+  }
+});
 
-//----user routes----//
+
+//----user route for testing purposes----//
 app.get('/users', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM users');
