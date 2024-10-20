10a5f96c97789194120d6a658c1ca6dc76307b76
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-20 14:13:10 -0400

    tidying notes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,13 +3,13 @@ const express = require('express');
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const router = express.Router();
 
-//route for user registration
+
 router.post('/register', registerUser);
 
-//route for user login
+
 router.post('/login', loginUser);
 
-//route for getting the user profile
+
 router.get('/profile', getUserProfile); 
 
 
