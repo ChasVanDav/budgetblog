72fff4102aef54de3f17d83e595b279c66f58658
ChasVanDav <blackkoreangirl@gmail.com>
2024-10-20 11:29:07 -0400

    starting from scratch with db connections and routes

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,15 +1,11 @@
-// Importing Pool from the 'pg' library to manage database connections.
 const { Pool } = require('pg'); 
-
-// Loads environment variables from a .env file.
 require('dotenv').config(); 
 
-// Using the database connection string from the .env file.
 const pool = new Pool({
-    user: "tpl1522_14",
+    user: "tpl522_14",
     host: "localhost",
     port: "5432",
-    database: "bugetblog"
+    database: "budgetblog"
 });
 
 // Exporting the pool to be used for database queries in other parts of the app.
